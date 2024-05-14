4aacde7d6bdbdb8f38982fb7c96aa8f5bdb4db62
Julio Barros <juliocezarbarros2@gmail.com>
2024-05-14 20:50:43 -0300

    Classe de representação da entidade Cliente

---
+++ src/main/java/org/juliobarros/model/Cliente.java
@@ -0,0 +1,31 @@
+package org.juliobarros.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String sobrenome;
+
+    @Column(nullable = false)
+    private Integer idade;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    private String email;
+
+    private String instagram;
+}
